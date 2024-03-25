0f34276495fdb77669dd2696864acde7336bfdae
Netanel-Iyov <nati16368447@gmail.com>
2024-03-25 06:24:42 +0000

    test with podTemplate

--- pipelines/HelloWorldPipeline.groovy
+++ pipelines/HelloWorldPipeline.groovy
@@ -1,15 +1,35 @@
-pipeline {
-    agent {
-        kubernetes {
-            inheritFrom 'jnlp-pod-agent'
-            defaultContainer 'jnlp'
-        }
-    }
+// pipeline {
+//     agent {
+//         kubernetes {
+//             inheritFrom 'jnlp-pod-agent'
+//             defaultContainer 'jnlp'
+//         }
+//     }
 
-    stages {
-        stage('Build') {
-            steps {
-                sh 'echo "Hello World"'
+//     stages {
+//         stage('Build') {
+//             steps {
+//                 sh 'echo "Hello World"'
+//             }
+//         }
+//     }
+// }
+pipeline {
+    agent none
+
+    podTemplate(containers: [
+        containerTemplate(name: 'jnlp', image: 'jenkins/inbound-agent:4.13.3-1', args: '${computer.jnlpmac} ${computer.name}')
+    ]) {
+        node(POD_LABEL) {
+            stages {
+                stage('Test') {
+                    steps {
+                        container('jnlp') {
+                            sh 'echo "Hello World"'
+                            sh "echo ${POD_LABEL}"
+                        }
+                    }
+                }
             }
         }
     }
