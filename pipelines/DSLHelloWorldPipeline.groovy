4f6b5a8d90c2f4921ab83bb19ed164d6202d53e0
Netanel-Iyov <nati16368447@gmail.com>
2024-05-02 17:44:27 +0000

    remove the sandbox - it is not corrrect syntax

--- pipelines/DSLHelloWorldPipeline.groovy
+++ pipelines/DSLHelloWorldPipeline.groovy
@@ -1,7 +1,6 @@
 // Uses Declarative syntax to run commands inside a container.
 pipelineJob('Test-Pipeline') {
     // TODO: remove sandbox
-    sandbox(boolean sandbox = true)
 
     definition {
         cps {
