ebd7b38a0c576a55e4e825ae150fe9560508ada6
Netanel-Iyov <nati16368447@gmail.com>
2024-05-16 19:02:38 +0000

    test

--- UpdateDNSRecords/UpdateDNSRecords.groovy
+++ UpdateDNSRecords/UpdateDNSRecords.groovy
@@ -31,7 +31,7 @@ spec:
         stage('Checkout') {
             steps {
                 cleanWs()
-                git credentialsId: 'Github-Credentials', url: 'https://github.com/Netanel-Iyov/Home-Server.git', branch: "${BRANCH}"
+                git credentialsId: 'Github-Credentials', url: 'https://github.com/Netanel-Iyov/Home-Server.git', branch: '${BRANCH}'
                 script {
                     sh 'ls -la'
                 }
@@ -42,7 +42,7 @@ spec:
             steps {
                 withCredentials([string(credentialsId: 'go-daddy-api-key', variable: 'api_key'), string(credentialsId: 'go-daddy-api-secret', variable: 'api_secret') ]) 
                 {
-                    sh "pip install requests && python3 ./misc/update_DNS_record.py --domain niyov.com --record-names ${RECORD_NAMES}"
+                    sh 'pip install requests && python3 ./misc/update_DNS_record.py --domain niyov.com --record-names ${RECORD_NAMES}'
                 }
             }
         }
