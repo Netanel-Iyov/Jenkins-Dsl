6588e066f89041238153b821aed033f7715b307b
Netanel-Iyov <nati16368447@gmail.com>
2024-05-16 18:41:36 +0000

    smal fix

--- TodoListCICD/DSLHelloWorldPipeline.groovy
+++
@@ -1,8 +0,0 @@
-// Uses Declarative syntax to run commands inside a container.
-pipelineJob('Todo-List-CICD') {
-    definition {
-        cps {
-            script(readFileFromWorkspace("./TodoListCICD/TodoLostCICD.groovy"))
-        }
-    }
-}
