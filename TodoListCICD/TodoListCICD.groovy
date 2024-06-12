1f4059367246658e9915c39c028ac80c5c15aec3
Netanel-Iyov <nati16368447@gmail.com>
2024-06-12 10:21:40 +0000

    extracting ci cd pipeline yaml to a different file

--- TodoListCICD/TodoListCICD.groovy
+++ TodoListCICD/TodoListCICD.groovy
@@ -1,40 +1,14 @@
 pipeline {
     agent {
         kubernetes {
-            // TODO: remove from here and extract to a different file
-            yaml '''
-apiVersion: v1
-kind: Pod
-spec:
-  containers:
-  - name: shell
-    image: ubuntu
-    command:
-    - sleep
-    args:
-    - infinity
-  - name: docker
-    image: docker:latest
-    command:
-    - "sleep"
-    args:
-    - "99d"
-    volumeMounts:
-    - name: docker-daemon
-      mountPath: /var/run/docker.sock
-    tty: true
-  volumes:
-  - name: docker-daemon
-    hostPath:
-      path: /var/run/docker.sock
-      type: Socket
-'''
+            yamlFile 'KubernetesPod.yaml'
             defaultContainer 'shell'
         }
     }
 
     environment {
-        DOCKER_IMAGE = "todolist-cicd-test:${BUILD_NUMBER}"
+        CLIENT_IMAGE_TAG=""
+        API_IMAGE_TAG=""
     }
 
     stages {
@@ -43,22 +17,49 @@ spec:
                 cleanWs()
                 git credentialsId: 'Github-Credentials', url: 'https://github.com/Netanel-Iyov/Todo-list'
                 script {
-                    sh 'ls -la'
+                    sh 'echo "Display Checkout content" && ls -la'
                 }
             }
+        }
+
+        stage ('Verify Versions') {
+            // Check github triggers and regexes
+            // 1. define the api and client tags, for example: 'natiiyov/todo-list-client:1.0.0'
+            // 2. get changed files and sort by /api, /client
+            // 3. extract versions from the versions.json file in the repository
+            // 4. for each tag see if changed files in the relevant directory, if changed check version
+            // 5. if both tags are valid then continue the pipeline.
+            // 6. if not valid, then notify somehow and exit the pipeline with Failed status code
         }
 
         // stage('Test') {
 
         // }
 
-        stage('Build & Push To Registry') {
+        stage('Build API & Push To Registry') {
             steps {
                 container('docker') {
                     dir('api') {
                         script {
                             sh 'ls -la'
-                            sh 'docker build -f Dockerfile.prod -t ${DOCKER_IMAGE} .'
+                            sh 'docker build -f Dockerfile.prod -t ${API_IMAGE_TAG} .'
+                            
+                            def dockerImage = docker.image("${DOCKER_IMAGE}")
+                            // docker.withRegistry('https://harbor.niyov.com', "harbor.niyov-credentials") {
+                                // dockerImage.push()
+                        }
+                    }
+                }
+            }
+        }
+
+        stage('Build Client & Push To Registry') {
+            steps {
+                container('docker') {
+                    dir('api') {
+                        script {
+                            sh 'ls -la'
+                            sh 'docker build -t natiiyov/todo-list-client:1.0.0 -f Dockerfile.prod .'
                             def dockerImage = docker.image("${DOCKER_IMAGE}")
                             // docker.withRegistry('https://harbor.niyov.com', "harbor.niyov-credentials") {
                                 // dockerImage.push()
