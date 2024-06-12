513a884bef9974d5cb1dfe974663d35c7e4ac296
Netanel-Iyov <nati16368447@gmail.com>
2024-06-12 19:39:49 +0000

    extending the pipeline and testing

--- TodoListCICD/TodoListCICD.groovy
+++ TodoListCICD/TodoListCICD.groovy
@@ -1,3 +1,24 @@
+import org.jenkinsci.plugins.pipeline.modeldefinition.Utils
+
+def applicationValues = [
+    'api' : [
+            'workdir': 'api',
+            'versionFile': './api/metadata.json',
+            'changesFilesRegex': 'api/*',
+            'imageName': 'natiiyov/todo-list-api',
+            'deploymentFile': 'api/deployment.yaml',
+            'imagePattern': ~/natiiyov\/todo-list-api:\d+\.\d+\.\d+/
+            ], 
+    'client' : [
+                'workdir': 'client'
+                'versionFile': './client/metadata.json',
+                'changesFilesRegex': 'client/*',
+                'imageName': 'natiiyov/todo-list-client',
+                'deploymentFile': 'client/deployment.yaml',
+                'imagePattern': ~/natiiyov\/todo-list-client:\d+\.\d+\.\d+/
+            ]
+]
+
 pipeline {
     agent {
         kubernetes {
@@ -6,18 +27,15 @@ pipeline {
         }
     }
 
-    environment {
-        CLIENT_IMAGE_TAG=""
-        API_IMAGE_TAG=""
-    }
-
     stages {
         stage('Checkout') {
             steps {
-                cleanWs()
-                git credentialsId: 'Github-Credentials', url: 'https://github.com/Netanel-Iyov/Todo-list'
-                script {
-                    sh 'echo "Display Checkout content" && ls -la'
+                dir('Todo-list') {
+                    cleanWs()
+                    git credentialsId: 'Github-Credentials', url: 'https://github.com/Netanel-Iyov/Todo-list', branch: 'test-CICD'
+                    script {
+                        sh 'echo "Display Checkout content" && ls -la'
+                    }
                 }
             }
         }
@@ -30,52 +48,118 @@ pipeline {
             // 4. for each tag see if changed files in the relevant directory, if changed check version
             // 5. if both tags are valid then continue the pipeline.
             // 6. if not valid, then notify somehow and exit the pipeline with Failed status code
+            steps {
+                dir('Todo-list') {
+                    script {
+                        applicationValues.each { appLabel, appData ->
+                            def jsonFile = readFile(file: appData.versionsFile)
+                            def jsonData = readJSON(text: jsonFile)
+
+                            def version = jsonData.version
+
+                            // check if changed files match 
+                            def changedFilesMatched = true
+                            def imageTag = "${appData.imageName}:${version}"
+                            def isImageExist = sh("docker manifest inspect ${imageTag} > /dev/null" == 0, returnStatus: true) ? true : false
+                            
+                            if (changedFilesMatched && !isImageExist) {
+                                appData['toDeploy'] = true
+                                appData['tag'] = imageTag
+                            } else {
+                                appData['toDeploy'] = false
+                                sh "echo 'Tag ${imageTag} already exist in docker registry. Please make sure you didnt forget to update the version in ${versionFile}'"
+                            }
+                        }
+                    }
+                }
+            }
         }
 
         // stage('Test') {
 
         // }
 
-        stage('Build API & Push To Registry') {
+        stage("Build API & Push To Registry") {
             steps {
-                container('docker') {
-                    dir('api') {
-                        script {
-                            sh 'ls -la'
-                            sh 'docker build -f Dockerfile.prod -t ${API_IMAGE_TAG} .'
-                            
-                            def dockerImage = docker.image("${DOCKER_IMAGE}")
-                            // docker.withRegistry('https://harbor.niyov.com', "harbor.niyov-credentials") {
-                                // dockerImage.push()
+                dir('Todo-list') {
+                    script {
+                        if (applicationValues.api.toDeploy) {
+                            container('docker') {
+                                dir(appData.workdir) {
+                                    docker.withRegistry('https://hub.docker.com', 'DockerHub-Credentials') {
+                                        def dockerImage = docker.build(applicationValues.api.tag, "-f Dockerfile.prod .")
+                                        // dockerImage.push()
+                                    }
+                                }
+                            }
+                        } else {
+                            Utils.markStageSkippedForConditional(STAGE_NAME)
                         }
                     }
                 }
             }
         }
 
-        stage('Build Client & Push To Registry') {
+        stage("Build Client & Push To Registry") {
             steps {
-                container('docker') {
-                    dir('api') {
-                        script {
-                            sh 'ls -la'
-                            sh 'docker build -t natiiyov/todo-list-client:1.0.0 -f Dockerfile.prod .'
-                            def dockerImage = docker.image("${DOCKER_IMAGE}")
-                            // docker.withRegistry('https://harbor.niyov.com', "harbor.niyov-credentials") {
-                                // dockerImage.push()
+                dir('Todo-list/client') {
+                    script {
+                        if (applicationValues.client.toDeploy) {
+                            container('docker') {
+                                dir(appData.workdir) {
+                                    docker.withRegistry('https://hub.docker.com', 'DockerHub-Credentials') {
+                                        def dockerImage = docker.build(applicationValues.client.tag, "-f Dockerfile.prod .")
+                                        // dockerImage.push()
+                                    }
+                                }
+                            }
+                        } else {
+                            Utils.markStageSkippedForConditional(STAGE_NAME)
                         }
                     }
                 }
             }
+        } 
+
+        stage('Checkout ArgoCD Repository') {
+            steps {
+                dir('argocd-todolist') {
+                    git credentialsId: 'Github-Credentials', url: 'https://github.com/Netanel-Iyov/todolist-argocd', branch: 'main'
+                }
+            }
         }
 
-        // stage('Update Deployment File') {
-        //     steps {
-        //         script {
-                    
-        //         }
-        //     }
-        // }
-        
+        stage('Update Deployment files') {
+            steps {
+                dir('argocd-todolist') {
+                    applicationValues.each { appLabel, appData ->
+                        if (!appData.toDeploy) {
+                            echo "Skipping ${appLabel} Deployment"
+                        }
+
+                        // Read the deployment.yaml file into deploymentFileData
+                        def deploymentFileData = readYaml file: appData.deploymentFile
+                        
+                        // Define the regex pattern for the image
+                        def pattern = appData.imagePattern
+                        
+                        // Iterate over the containers and update the image for the specified container
+                        def containers = deploymentFileData.spec.template.spec.containers
+                        containers.each { container ->
+                            if (container.image ==~ pattern) {
+                                container.image = appData.tag
+                            }
+                        }
+                        
+                        // Write the modified deploymentFileData back to the deployment.yaml file
+                        def newDeploymentFileContent = writeYaml file: appData.deploymentFile, data: deploymentFileData, overwrite: true, returnText: true
+
+                        echo "${appLabel} Deployment file contents are: ${newDeploymentFileContent}"
+                    }
+
+                    // commit and push
+                }
+            }
+        }
     }
 }
