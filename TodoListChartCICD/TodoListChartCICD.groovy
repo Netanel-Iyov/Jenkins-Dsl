470916f3ac99695131b5bd011d9a6e589d383a35
Netanel-Iyov <nati16368447@gmail.com>
2025-01-07 20:10:42 +0000

    small fix

--- TodoListChartCICD/TodoListChartCICD.groovy
+++ TodoListChartCICD/TodoListChartCICD.groovy
@@ -11,7 +11,7 @@ pipeline {
             steps {
                 dir('Todo-list') {
                     cleanWs()
-                    checkout scmGit(branches: [[name: 'refs/tags/${RELEASE_TAG}']], userRemoteConfigs: [[credentialsId: 'Github-Credentials', url: 'https://github.com/Netanel-Iyov/Todo-list']])
+                    checkout scmGit(branches: [[name: "refs/tags/${RELEASE_TAG}"]], userRemoteConfigs: [[credentialsId: 'Github-Credentials', url: 'https://github.com/Netanel-Iyov/Todo-list']])
                     script {
                         sh 'echo "Display Checkout content" && ls -la'
                     }
