bdce06e147072db12abc29769b3aaee901658f39
Netanel-Iyov <nati16368447@gmail.com>
2025-01-07 20:09:59 +0000

    test podFile definition extraction

--- TodoListChartCICD/TodoListChartCICD.groovy
+++ TodoListChartCICD/TodoListChartCICD.groovy
@@ -1,50 +1,12 @@
 pipeline {
     agent {
         kubernetes {
-            yaml '''
-apiVersion: v1
-kind: Pod
-spec:
-  containers:
-    - name: git
-      image: alpine/git:2.45.2
-      command:
-        - sleep
-      args:
-        - infinity
-    - name: shell
-      image: ubuntu
-      command:
-        - sleep
-      args:
-        - infinity
-    - name: docker
-      image: docker:latest
-      command:
-        - "sleep"
-      args:
-        - "99d"
-      volumeMounts:
-        - name: docker-daemon
-          mountPath: /var/run/docker.sock
-      tty: true
-  volumes:
-    - name: docker-daemon
-      hostPath:
-        path: /var/run/docker.sock
-        type: Socket
-            '''
+            yaml readFile('TodoListChartCiCD/K8SPod.yaml')
             defaultContainer 'shell'
         }
     }
 
     stages {
-        stage('Debug') {
-            steps {
-                echo "${env.ACTION}"
-                echo "${env.RELEASE_TAG}"
-            }
-        }
         stage('Checkout') {
             steps {
                 dir('Todo-list') {
