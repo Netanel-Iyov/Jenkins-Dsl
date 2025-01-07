1ff2f22fbe95426c39cf403afb352e08baaf9c42
Netanel-Iyov <nati16368447@gmail.com>
2025-01-07 20:50:59 +0000

    reverting back

--- TodoListChartCICD/TodoListChartCICD.groovy
+++ TodoListChartCICD/TodoListChartCICD.groovy
@@ -1,7 +1,39 @@
 pipeline {
     agent {
         kubernetes {
-            yamlFile 'TodoListChartCICD/K8SPod.yaml'
+            yamlFile '''
+apiVersion: v1
+kind: Pod
+spec:
+  containers:
+    - name: git
+      image: alpine/git:2.45.2
+      command:
+        - sleep
+      args:
+        - infinity
+    - name: shell
+      image: ubuntu
+      command:
+        - sleep
+      args:
+        - infinity
+    - name: docker
+      image: docker:latest
+      command:
+        - "sleep"
+      args:
+        - "99d"
+      volumeMounts:
+        - name: docker-daemon
+          mountPath: /var/run/docker.sock
+      tty: true
+  volumes:
+    - name: docker-daemon
+      hostPath:
+        path: /var/run/docker.sock
+        type: Socket
+'''
             defaultContainer 'shell'
         }
     }
