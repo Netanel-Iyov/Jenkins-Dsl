a40e2726230213984074dbad70f04d01384b119c
Netanel-Iyov <nati16368447@gmail.com>
2025-01-09 22:06:11 +0000

    test

--- TodoListChartCICD/TodoListChartCICD.groovy
+++ TodoListChartCICD/TodoListChartCICD.groovy
@@ -178,7 +178,7 @@ spec:
                         yamlContent = yamlContent.replaceAll("${DOCKER_REPOSITORY}\todo-list-api.*:\\d+\\.\\d+\\.\\d+", env.API_TAG)
                         
                         // Write the modified YAML content back to the file
-                        writeFile file: yamlFilePath, text: yamlContent, overwrite: true
+                        writeFile file: fileToUpdate, text: yamlContent, overwrite: true
                         
                         echo "${fileToUpdate} file has been updated with new versions..."
                         sh "cat ${fileToUpdate}"
