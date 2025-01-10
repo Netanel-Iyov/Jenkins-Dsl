9f39d4cacaa3e878b7ed789fa97c6c13fbd5ada3
Netanel-Iyov <nati16368447@gmail.com>
2025-01-10 08:32:17 +0000

    refrencing podfile

--- TodoListChartCICD/TodoListChartCICD.groovy
+++ TodoListChartCICD/TodoListChartCICD.groovy
@@ -1,39 +1,7 @@
 pipeline {
     agent {
         kubernetes {
-            yaml '''
-apiVersion: v1
-kind: Pod
-spec:
-  containers:
-    - name: git
-      image: alpine/git:2.45.2
-      command:
-        - sleep
-      args:
-        - infinity
-    - name: shell
-      image: ubuntu
-      command:
-        - sleep
-      args:
-        - infinity
-    - name: docker
-      image: docker:latest
-      command:
-        - "sleep"
-      args:
-        - "99d"
-      volumeMounts:
-        - name: docker-daemon
-          mountPath: /var/run/docker.sock
-      tty: true
-  volumes:
-    - name: docker-daemon
-      hostPath:
-        path: /var/run/docker.sock
-        type: Socket
-            '''
+            yamlFile 'K8SPod.yaml'
             defaultContainer 'shell'
         }
     }
